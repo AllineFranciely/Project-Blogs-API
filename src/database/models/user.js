083a2cca1ae720e7e06bae7fb360b011ee6710c1
Alline <alline_franciely@hotmail.com>
2022-07-13 17:49:36 -0300

    requisito 2 again

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,20 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  const TableUsers = sequelize.define('User', {
+  const UserTable = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
     },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING
+    password: DataTypes.INTEGER,
+    image: DataTypes.STRING,
   },
-    {
-      timestamps: false,
-    });
-  TableUsers.associate = (models) => {
-    TableUsers.hasMany(models.BlogPost, { foreignKey: "userId", as: "posts" });
-  }
-  return TableUsers;
+  {
+    timestamps: false,
+    tableName: 'Users',
+  });
+  return UserTable;
 };
